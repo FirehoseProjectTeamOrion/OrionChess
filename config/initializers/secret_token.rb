ba0453248d19fb49c345cf340d3468b824bcbfbf
John K. Towery <kbaries@gmail.com>
2016-01-24 21:16:27 -0700

    Initial Commit
    just created app with:
    
    rails new OrionChess --database=postgresql
    
    need to update database.yml with proper password

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OrionChess::Application.config.secret_key_base = 'cd4aca0e771a7f1e0efa85bd0b19fafa83f9e32ff404efa9d2abf2b46c93a597ec848ba6fe26191f801a0f336246b236e4a4372fc6da061bd95b695c920e5425'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OrionChess::Application.config.session_store :cookie_store, key: '_OrionChess_session'
