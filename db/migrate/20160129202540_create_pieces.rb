df346ffa816b75f76d61e4983df6e9112ce90b47
Matthew Meitl <DarkSamus6@gmail.com>
2016-01-29 21:17:58 +0000

    setup migrations

---
+++ db/migrate/20160129202540_create_pieces.rb
@@ -0,0 +1,18 @@
+class CreatePieces < ActiveRecord::Migration
+  def change
+    create_table :pieces do |t|
+      t.string :type
+      t.integer :row
+      t.integer :column
+      t.boolean :in_game
+      t.integer :player_id
+      t.integer :game_id      
+
+      t.timestamps
+    end
+    
+    add_index :pieces, :game_id
+    add_index :pieces, :player_id
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126200337) do
+ActiveRecord::Schema.define(version: 20160129204711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pieces", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "players", force: true do |t|
+    t.integer  "player_id"
+    t.string   "name"
+    t.integer  "wins"
+    t.integer  "losses"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
