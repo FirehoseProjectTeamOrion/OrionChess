c4ea495c5bd7dc1ec5af00b1d607cdc529c6c7a5
John Kelly Towery <kbaries@gmail.com>
2016-02-01 17:42:13 +0000

    Add STI table

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,11 @@
 class Piece < ActiveRecord::Base
     belongs_to :player
-    belongs_to :game    
+    belongs_to :game
 end
+
+class Pawn < Piece; end
+class Knight < Piece; end
+class Bishop < Piece; end
+class Rook < Piece; end
+class Queen < Piece; end
+class King < Piece; end
