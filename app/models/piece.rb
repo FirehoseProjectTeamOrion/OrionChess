8e4e56b789e7ce7576da5a71aa86d88510d7f6c6
John Kelly Towery <kbaries@gmail.com>
2016-02-02 20:44:06 +0000

    Moved the 'scope' line to inside class/end block

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,11 +1,13 @@
 class Piece < ActiveRecord::Base
   belongs_to :player
   belongs_to :game
-end
 
-scope :pawns, -> { where(race: 'Pawn') }
-scope :knights, -> { where(race: 'Knight') }
-scope :bishops, -> { where(race: 'Bishop') }
-scope :rooks, -> { where(race: 'Rook') }
-scope :queens, -> { where(race: 'Queen') }
-scope :kings, -> { where(race: 'King') }
+
+scope :pawns, -> { where(type: 'Pawn') }
+scope :knights, -> { where(type: 'Knight') }
+scope :bishops, -> { where(type: 'Bishop') }
+scope :rooks, -> { where(type: 'Rook') }
+scope :queens, -> { where(type: 'Queen') }
+scope :kings, -> { where(type: 'King') }
+
+end
