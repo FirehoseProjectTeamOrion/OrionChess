097b46413c45115e12ac4cc73430af406eaa0e6e
John Kelly Towery <kbaries@gmail.com>
2016-02-02 21:19:56 +0000

    Clean up

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,12 +2,10 @@ class Piece < ActiveRecord::Base
   belongs_to :player
   belongs_to :game
 
-
-scope :pawns, -> { where(type: 'Pawn') }
-scope :knights, -> { where(type: 'Knight') }
-scope :bishops, -> { where(type: 'Bishop') }
-scope :rooks, -> { where(type: 'Rook') }
-scope :queens, -> { where(type: 'Queen') }
-scope :kings, -> { where(type: 'King') }
-
+  scope :pawns, -> { where(type: 'Pawn') }
+  scope :knights, -> { where(type: 'Knight') }
+  scope :bishops, -> { where(type: 'Bishop') }
+  scope :rooks, -> { where(type: 'Rook') }
+  scope :queens, -> { where(type: 'Queen') }
+  scope :kings, -> { where(type: 'King') }
 end
