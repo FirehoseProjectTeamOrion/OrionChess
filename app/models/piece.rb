806a49451933378e3d91a3be7cc39cefed37e098
Sean Lovinger <lovingersean01@gmail.com>
2016-02-04 08:16:32 +0000

    Got a "rough draft" of is_obstructed? implemented, added a few more tests and updated piece factory
    I Got an  is_obstructed? method put togther that does successfully pass tests but seems like it could probably be cleaned up a bit. I did separate the different cases into different methods as suggested but I'm not sure if the way I went through to check if the spaces in between were occupied was the best way or not. I had two ideas, either to loop through all the pieces and see if any of them were in the way or just check the spaces along the path and see if any of them are occupied. I eventually decided to do the latter of the two since it seemed like it made more sense only to check the spaces we care about instead of looking at every piece. This method worked pretty well, I think, for horizontal and vertical paths but for diaganol paths it was a little more complicated. So not sure if maybe the other way would've been better or if I just need to rethink of how to do my loops. Along with that, there also seem like there might be a DRY issue with the way it is now since there is a lot of similar code being used in multiple spots so might need see if there is away to fix that up as well. Other than that it seems to be working at the moment so we at least have something usable that can be cleaned up if needed.
    
    I added some more tests to to test all the different directions (left, right, up, down, up and right, up and left, down and right, down and left) and I also added a test for when the piece does not move.
    
    Found out that I needed to capitalize the q in queen for the type of the piece so the STI would work

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -8,4 +8,83 @@ class Piece < ActiveRecord::Base
   scope :rooks, -> { where(type: 'Rook') }
   scope :queens, -> { where(type: 'Queen') }
   scope :kings, -> { where(type: 'King') }
+
+  def is_obstructed?(row, column)
+    if moving_horizontally?(row)
+      is_horizontal_obstructed?(column)
+    elsif moving_vertically?(column)
+      is_vertical_obstructed?(row)
+    elsif moving_diagonally?(row, column)
+      is_diagonal_obstructed?(row, column)
+    else
+      raise "Input is invalid!"
+    end
+  end
+
+  private
+
+  def moving_horizontally?(row)
+    self.row == row
+  end
+
+  def moving_vertically?(column)
+    self.column == column
+  end
+
+  def moving_diagonally?(row, column)
+    (self.row - row).abs == (self.column - column).abs
+  end
+
+  def is_horizontal_obstructed?(column)
+    obstructed = false
+    if self.column < column
+      (self.column+1...column).each do |x_pos|
+        obstructed |= game.pieces.find_by(row: self.row, column: x_pos).present?
+      end
+    else
+      (column+1...self.column).each do |x_pos|
+        obstructed |= game.pieces.find_by(row: self.row, column: x_pos).present?
+      end
+    end
+
+    obstructed
+  end
+
+  def is_vertical_obstructed?(row)
+    obstructed = false
+    if self.row < row
+      (self.row+1...row).each do |y_pos|
+        obstructed |= game.pieces.find_by(row: y_pos, column: self.column).present?
+      end
+    else
+      (row+1...self.row).each do |y_pos|
+        obstructed |= game.pieces.find_by(row: y_pos, column: self.column).present?
+      end
+    end
+
+    obstructed
+  end
+
+  def is_diagonal_obstructed?(row, column)
+    obstructed = false
+    if self.row < row && self.column < column
+      (1...row-self.row).each do |n|
+        obstructed |= game.pieces.find_by(row: self.row+n, column: self.column+n).present?
+      end
+    elsif self.row < row && self.column > column
+      (1...row-self.row).each do |n|
+        obstructed |= game.pieces.find_by(row: self.row+n, column: self.column-n).present?
+      end
+    elsif self.row > row && self.column < column
+      (1...self.row-row).each do |n|
+        obstructed |= game.pieces.find_by(row: self.row-n, column: self.column+n).present?
+      end
+    else
+      (1...self.row-row).each do |n|
+        obstructed |= game.pieces.find_by(row: self.row-n, column: self.column-n).present?
+      end
+    end
+
+    obstructed
+  end
 end
