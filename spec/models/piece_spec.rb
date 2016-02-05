0bb4fa246dc8db74af6caf161b623c7f84ab8f56
Sean Lovinger <lovingersean01@gmail.com>
2016-02-05 07:02:11 +0000

    Made made suggested changes from code review
    - Added more descriptive variables, got rid of abbreviations and removed self
    - Fixed issues with rubocop(removed "is_" from methods, added gaurd cases and fixed the carriage return failure)
    - Added # to describe for obstructed? instance method
    - Removed should from "it" statements in piece_spec.rb
    - Changed piece1 to be just piece
    - Refactored check_path using a return and if statement

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,124 +1,124 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  describe 'is_obstructed?' do
-    it 'should return false for no horizontal obstructions moving to the right' do
+  describe '#obstructed?' do
+    it 'returns false for no horizontal obstructions moving to the right' do
       piece = FactoryGirl.create(:piece, row: 0, column: 3)
 
-      expect(piece.is_obstructed?(0, 5)).to eq(false)
+      expect(piece.obstructed?(0, 5)).to eq(false)
     end
 
-    it 'should return false for no horizontal obstructions moving to the left' do
+    it 'returns false for no horizontal obstructions moving to the left' do
       piece = FactoryGirl.create(:piece, row: 0, column: 3)
 
-      expect(piece.is_obstructed?(0, 1)).to eq(false)
+      expect(piece.obstructed?(0, 1)).to eq(false)
     end
 
-    it 'should return true for horizontal obstructions moving to the right' do
-      piece1 = FactoryGirl.create(:piece, row: 0, column: 3)
-      FactoryGirl.create(:piece, row: 0, column: 4, game: piece1.game)
+    it 'returns true for horizontal obstructions moving to the right' do
+      piece = FactoryGirl.create(:piece, row: 0, column: 3)
+      FactoryGirl.create(:piece, row: 0, column: 4, game: piece.game)
 
-      expect(piece1.is_obstructed?(0, 5)).to eq(true)
+      expect(piece.obstructed?(0, 5)).to eq(true)
     end
 
-    it 'should return true for horizontal obstructions moving to the left' do
-      piece1 = FactoryGirl.create(:piece, row: 0, column: 3)
-      FactoryGirl.create(:piece, row: 0, column: 2, game: piece1.game)
+    it 'returns true for horizontal obstructions moving to the left' do
+      piece = FactoryGirl.create(:piece, row: 0, column: 3)
+      FactoryGirl.create(:piece, row: 0, column: 2, game: piece.game)
 
-      expect(piece1.is_obstructed?(0, 1)).to eq(true)
+      expect(piece.obstructed?(0, 1)).to eq(true)
     end
 
-    it 'should return false for no vertical obstructions moving up' do
+    it 'returns false for no vertical obstructions moving up' do
       piece = FactoryGirl.create(:piece, row: 3, column: 5)
 
-      expect(piece.is_obstructed?(6, 5)).to eq(false)
+      expect(piece.obstructed?(6, 5)).to eq(false)
     end
 
-    it 'should return false for no vertical obstructions moving down' do
+    it 'returns false for no vertical obstructions moving down' do
       piece = FactoryGirl.create(:piece, row: 3, column: 5)
 
-      expect(piece.is_obstructed?(0, 5)).to eq(false)
+      expect(piece.obstructed?(0, 5)).to eq(false)
     end
 
-    it 'should return true for vertical obstructions moving up' do
-      piece1 = FactoryGirl.create(:piece, row: 3, column: 5)
-      FactoryGirl.create(:piece, row: 4, column: 5, game: piece1.game)
+    it 'returns true for vertical obstructions moving up' do
+      piece = FactoryGirl.create(:piece, row: 3, column: 5)
+      FactoryGirl.create(:piece, row: 4, column: 5, game: piece.game)
 
-      expect(piece1.is_obstructed?(6, 5)).to eq(true)
+      expect(piece.obstructed?(6, 5)).to eq(true)
     end
 
-    it 'should return true for vertical obstructions moving down' do
-      piece1 = FactoryGirl.create(:piece, row: 3, column: 5)
-      FactoryGirl.create(:piece, row: 1, column: 5, game: piece1.game)
+    it 'returns true for vertical obstructions moving down' do
+      piece = FactoryGirl.create(:piece, row: 3, column: 5)
+      FactoryGirl.create(:piece, row: 1, column: 5, game: piece.game)
 
-      expect(piece1.is_obstructed?(0, 5)).to eq(true)
+      expect(piece.obstructed?(0, 5)).to eq(true)
     end
 
-    it 'should return false for no diagonal obstructions moving up and to the right' do
+    it 'returns false for no diagonal obstructions moving up and to the right' do
       piece = FactoryGirl.create(:piece, row: 2, column: 2)
-      expect(piece.is_obstructed?(4, 4)).to eq(false)
+      expect(piece.obstructed?(4, 4)).to eq(false)
     end
 
-    it 'should return false for no diagonal obstructions moving up and to the left' do
+    it 'returns false for no diagonal obstructions moving up and to the left' do
       piece = FactoryGirl.create(:piece, row: 2, column: 2)
-      expect(piece.is_obstructed?(4, 0)).to eq(false)
+      expect(piece.obstructed?(4, 0)).to eq(false)
     end
 
-    it 'should return false for no diagonal obstructions moving down and to the right' do
+    it 'returns false for no diagonal obstructions moving down and to the right' do
       piece = FactoryGirl.create(:piece, row: 2, column: 2)
-      expect(piece.is_obstructed?(0, 4)).to eq(false)
+      expect(piece.obstructed?(0, 4)).to eq(false)
     end
 
-    it 'should return false for no diagonal obstructions moving down and to the left' do
+    it 'returns false for no diagonal obstructions moving down and to the left' do
       piece = FactoryGirl.create(:piece, row: 2, column: 2)
-      expect(piece.is_obstructed?(0, 0)).to eq(false)
+      expect(piece.obstructed?(0, 0)).to eq(false)
     end
 
-    it 'should return true for diagonal obstructions moving up and to the right' do
-      piece1 = FactoryGirl.create(:piece, row: 2, column: 2)
-      FactoryGirl.create(:piece, row: 3, column: 3, game: piece1.game)
+    it 'returns true for diagonal obstructions moving up and to the right' do
+      piece = FactoryGirl.create(:piece, row: 2, column: 2)
+      FactoryGirl.create(:piece, row: 3, column: 3, game: piece.game)
 
-      expect(piece1.is_obstructed?(4, 4)).to eq(true)
+      expect(piece.obstructed?(4, 4)).to eq(true)
     end
 
-    it 'should return true for diagonal obstructions moving up and to the left' do
-      piece1 = FactoryGirl.create(:piece, row: 2, column: 2)
-      FactoryGirl.create(:piece, row: 3, column: 1, game: piece1.game)
+    it 'returns true for diagonal obstructions moving up and to the left' do
+      piece = FactoryGirl.create(:piece, row: 2, column: 2)
+      FactoryGirl.create(:piece, row: 3, column: 1, game: piece.game)
 
-      expect(piece1.is_obstructed?(4, 0)).to eq(true)
+      expect(piece.obstructed?(4, 0)).to eq(true)
     end
 
-    it 'should return true for diagonal obstructions moving down and to the right' do
-      piece1 = FactoryGirl.create(:piece, row: 2, column: 2)
-      FactoryGirl.create(:piece, row: 1, column: 3, game: piece1.game)
+    it 'returns true for diagonal obstructions moving down and to the right' do
+      piece = FactoryGirl.create(:piece, row: 2, column: 2)
+      FactoryGirl.create(:piece, row: 1, column: 3, game: piece.game)
 
-      expect(piece1.is_obstructed?(0, 4)).to eq(true)
+      expect(piece.obstructed?(0, 4)).to eq(true)
     end
 
-    it 'should return true for diagonal obstructions moving down and to the left' do
-      piece1 = FactoryGirl.create(:piece, row: 2, column: 2)
-      FactoryGirl.create(:piece, row: 1, column: 1, game: piece1.game)
+    it 'returns true for diagonal obstructions moving down and to the left' do
+      piece = FactoryGirl.create(:piece, row: 2, column: 2)
+      FactoryGirl.create(:piece, row: 1, column: 1, game: piece.game)
 
-      expect(piece1.is_obstructed?(0, 0)).to eq(true)
+      expect(piece.obstructed?(0, 0)).to eq(true)
     end
 
-    it 'should return false when there is a piece at the destination and none in between' do
-      piece1 = FactoryGirl.create(:piece, row: 0, column: 3)
-      FactoryGirl.create(:piece, row: 0, column: 5, game: piece1.game)
+    it 'returns false when there is a piece at the destination and none in between' do
+      piece = FactoryGirl.create(:piece, row: 0, column: 3)
+      FactoryGirl.create(:piece, row: 0, column: 5, game: piece.game)
 
-      expect(piece1.is_obstructed?(0, 5)).to eq(false)
+      expect(piece.obstructed?(0, 5)).to eq(false)
     end
 
-    it "should return false if piece doesn't move" do
+    it "returns false if piece doesn't move" do
       piece = FactoryGirl.create(:piece, row: 0, column: 3)
 
-      expect(piece.is_obstructed?(0, 3)).to eq(false)
+      expect(piece.obstructed?(0, 3)).to eq(false)
     end
 
-    it 'should raise an error message if input is invalid' do
+    it 'raises an error message if input is invalid' do
       piece = FactoryGirl.create(:piece, row: 0, column: 3)
 
-      expect { piece.is_obstructed?(2, 4) }.to raise_error('Input is invalid!')
+      expect { piece.obstructed?(2, 4) }.to raise_error('Input is invalid!')
     end
   end
 end
