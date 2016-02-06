031966058ab2b8b8e2038ef5c6777faba94c6585
Will Cenk <willcenk@gmail.com>
2016-02-06 21:03:56 +0000

    Add available scope to Games

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,5 @@ class Game < ActiveRecord::Base
   has_many :pieces
   has_one  :white_player, class_name: 'User'
   has_one  :black_player, class_name: 'User'
+  scope :available, -> {where('black_player_id IS NULL OR white_player_id IS NULL'}
 end
