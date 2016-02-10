3fcb8ed87bde17b513666dd670a9aa87ee6c584d
Sean Lovinger <lovingersean01@gmail.com>
2016-02-10 19:46:20 +0000

    Fixing up view tests and refactoring helper tests
    * Adds the rspec-html-matchers gem to help with view tests, now we can check for the specific number of pieces
    * Removes unnecessary commented out code in games_helper_spec.rb
    * DRYed up games_helper_spec.rb by using let(:game) { FactoryGirl.create(:game) } to create a game for all tests

--- spec/views/games/show.html.erb_spec.rb
+++ spec/views/games/show.html.erb_spec.rb
@@ -5,83 +5,107 @@ RSpec.describe 'games/show.html.erb', type: :view do
     assign(:game, FactoryGirl.create(:game))
     render
 
-    expect(rendered).to include('p')
+    expect(rendered).to have_tag('div', :with => { :class => 'chessboard' }) do
+      with_tag 'div', :text => 'p', :count => 8
+    end
   end
 
   it 'has 8 black pawns(o) on the board' do
     assign(:game, FactoryGirl.create(:game))
     render
 
-    expect(rendered).to include('o')
+    expect(rendered).to have_tag('div', :with => { :class => 'chessboard' }) do
+      with_tag 'div', :text => 'o', :count => 8
+    end
   end
 
   it 'has 2 white knights(n) on the board' do
     assign(:game, FactoryGirl.create(:game))
     render
 
-    expect(rendered).to include('n')
+    expect(rendered).to have_tag('div', :with => { :class => 'chessboard' }) do
+      with_tag 'div', :text => 'n', :count => 2
+    end
   end
 
   it 'has 2 black knights(m) on the board' do
     assign(:game, FactoryGirl.create(:game))
     render
 
-    expect(rendered).to include('m')
+    expect(rendered).to have_tag('div', :with => { :class => 'chessboard' }) do
+      with_tag 'div', :text => 'm', :count => 2
+    end
   end
 
   it 'has 2 white bishops(b) on the board' do
     assign(:game, FactoryGirl.create(:game))
     render
 
-    expect(rendered).to include('b')
+    expect(rendered).to have_tag('div', :with => { :class => 'chessboard' }) do
+      with_tag 'div', :text => 'b', :count => 2
+    end
   end
 
   it 'has 2 black bishops(v) on the board' do
     assign(:game, FactoryGirl.create(:game))
     render
 
-    expect(rendered).to include('p')
+    expect(rendered).to have_tag('div', :with => { :class => 'chessboard' }) do
+      with_tag 'div', :text => 'v', :count => 2
+    end
   end
 
   it 'has 2 white rooks(r) on the board' do
     assign(:game, FactoryGirl.create(:game))
     render
 
-    expect(rendered).to include('r')
+    expect(rendered).to have_tag('div', :with => { :class => 'chessboard' }) do
+      with_tag 'div', :text => 'r', :count => 2
+    end
   end
 
   it 'has 2 black rooks(t) on the board' do
     assign(:game, FactoryGirl.create(:game))
     render
 
-    expect(rendered).to include('t')
+    expect(rendered).to have_tag('div', :with => { :class => 'chessboard' }) do
+      with_tag 'div', :text => 't', :count => 2
+    end
   end
 
   it 'has 1 white queen(q) on the board' do
     assign(:game, FactoryGirl.create(:game))
     render
 
-    expect(rendered).to include('q')
+    expect(rendered).to have_tag('div', :with => { :class => 'chessboard' }) do
+      with_tag 'div', :text => 'q', :count => 1
+    end
   end
 
   it 'has 1 black queen(w) on the board' do
     assign(:game, FactoryGirl.create(:game))
     render
 
-    expect(rendered).to include('w')
+    expect(rendered).to have_tag('div', :with => { :class => 'chessboard' }) do
+      with_tag 'div', :text => 'w', :count => 1
+    end
   end
 
   it 'has 1 white king(k) on the board' do
     assign(:game, FactoryGirl.create(:game))
     render
 
-    expect(rendered).to include('k')
+    expect(rendered).to have_tag('div', :with => { :class => 'chessboard' }) do
+      with_tag 'div', :text => 'k', :count => 1
+    end
   end
 
   it 'has 1 black king(l) on the board' do
     assign(:game, FactoryGirl.create(:game))
     render
 
-    expect(rendered).to include('l')
+    expect(rendered).to have_tag('div', :with => { :class => 'chessboard' }) do
+      with_tag 'div', :text => 'l', :count => 1
+    end
   end
 end
