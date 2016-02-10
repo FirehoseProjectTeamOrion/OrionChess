eab69d79f2c0fad42f0e220c2b2e69b426c7f1c3
John Towery <kbaries@gmail.com>
2016-02-10 05:36:46 +0000

    Rubocop cleanup..refactored king.rb

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,26 +1,16 @@
 class King < Piece
-
-  def valid_move?(destination_row, destination_col)
-    if kings_direction.include?(direction)
-      return true
-    else
-      return false
-    end
+  def valid_move?(_destination_row, _destination_col)
+    kings_direction = [
+      [1, 0],
+      [-1, 0],
+      [0, -1],
+      [0, 1],
+      [1, -1],
+      [1, 1],
+      [-1, -1],
+      [-1, 1]
+    ]
+    return true if kings_direction.include?(direction)
+    false
   end
-
-
-  private
-
-  # which direction is valid for king
-  kings_direction = [
-    [1, 0],
-    [-1, 0],
-    [0, -1],
-    [0, 1],
-    [1, -1],
-    [1, 1],
-    [-1, -1],
-    [-1, 1]
-  ]
-
 end
