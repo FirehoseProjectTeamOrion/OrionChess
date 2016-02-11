5a39e634421fc022f6d49e5fe244a77072a0e789
John Towery <kbaries@gmail.com>
2016-02-11 21:51:01 +0000

    Set up king factories and test

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Piece, type: :model do
+RSpec.describe King, type: :model do
   describe '#valid_move?' do
+
     it 'return true for valid move' do
-      king = FactoryGirl.create(:piece, type: 'King', row: 0, column: 4)
+      king = FactoryGirl.create(:king)
       expect(king.valid_move?(1, 3)).to eq(true)
     end
 
     it 'return false for not valid move' do
-      king = FactoryGirl.create(:piece, type: 'King', row: 0, column: 4)
+      king = FactoryGirl.create(:king)
       expect(king.valid_move?(2, 6)).to eq(false)
     end
+
   end
 end
