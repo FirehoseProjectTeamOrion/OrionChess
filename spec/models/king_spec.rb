869770df075188774fad24b9c7bcb071edca965d
John Towery <kbaries@gmail.com>
2016-02-11 21:54:21 +0000

    Rubocop cleanup

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe King, type: :model do
   describe '#valid_move?' do
-
     it 'return true for valid move' do
       king = FactoryGirl.create(:king)
       expect(king.valid_move?(1, 3)).to eq(true)
@@ -12,6 +11,5 @@ RSpec.describe King, type: :model do
       king = FactoryGirl.create(:king)
       expect(king.valid_move?(2, 6)).to eq(false)
     end
-
   end
 end
