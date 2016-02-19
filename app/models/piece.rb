4b498aa483a705e7a674a3fcf603d3fcdd9f0e37
John Towery <kbaries@gmail.com>
2016-02-19 06:39:29 +0000

    clean up

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -20,22 +20,22 @@ class Piece < ActiveRecord::Base
   def move_to!(destination_row, destination_column)
       # byebug
   # practice line begin here
-    
+
     if occupied_space?(destination_row, destination_column) && capturable?(destination_row, destination_column)
       game.pieces.where(row: :destination_row, column: :destination_column).update_attributes(row: nil, column: nil, in_game: false) #not sure whats proper...just woke up hour ago   :)
      #^^^^^ this is opponent pieces ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-     
+
       update_attributes(row: destination_row, column: destination_column) #move our pieces to that new location after removing opponent
       return true
     elsif !(occupied_space?(destination_row, destination_column))
       update_attributes(row: destination_row, column: destination_column)
       return true
-    else 
+    else
       return false
     end
   end
-  
-  
+
+
 
 
 
@@ -63,22 +63,11 @@ class Piece < ActiveRecord::Base
  #  end
 #  end
 
-<<<<<<< HEAD
   def occupied_space?(destination_row, destination_column)
     return true if game.pieces.where(row: destination_row, column: destination_column).exists?
   end
 
   def capturable?(row, column)
-=======
-  def occupied_space?(d_row, d_column)
-    game.pieces.where(row: d_row, column: d_column).exists?
-    
-  end
-
-  def capturable?(row, column)
-    
-    
->>>>>>> e286a0a79f39542065599e5dea29e6b8d0799b72
     game.pieces.where(row: row, column: column, in_game: true).where.not(color: color).exists?
   end
 
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -7,38 +7,35 @@ RSpec.describe Piece, type: :model do
   end
 
   describe '#move_to!' do
-<<<<<<< HEAD
+
     before :each do
       game = FactoryGirl.create(:game)
       @piece_white = FactoryGirl.create(:piece, row: 0, column: 0, color: 'white', in_game: true, game_id: game.id)
       @piece_black = FactoryGirl.create(:piece, row: 0, column: 7, color: 'black', in_game: true, game_id: game.id)
     end
-=======
-   let(:piece_black) { FactoryGirl.create(:piece, row: 0, column: 7, color: 'black') }
-   let(:piece_white) { FactoryGirl.create(:piece, row: 0, column: 7, color: 'white') }
->>>>>>> e286a0a79f39542065599e5dea29e6b8d0799b72
-    it 'successfully moved the pieces to destination' do
-      
-      expect(@piece_white.move_to!(0,7)).to eq(true)
-      
 
-      
+    it 'successfully moved the pieces to destination' do
+
+      expect(@piece_white.move_to!(0,7)).to eq(true)
+
+
+
     end
 
     it 'should show captured pieces to be out of game' do
-      
+
       @piece_white.move_to!(0, 7)
       expect(@piece_black.in_game).to eq(false)
     end
-    
+
     it 'should return false because piece is our own' do
-      
+
       white_piece = FactoryGirl.create(:piece, row: 4, column: 4, color: 'white')
       black_piece = FactoryGirl.create(:piece, row: 3, column: 4, color: 'black')
-      
+
       expect(white_piece.move_to!(black_piece.row, black_piece.column)).to eq(false)
-      
-    end    
+
+    end
 
 
   end
