956e9dc84c45056d5b0980576b25121280f2185b
John Towery <kbaries@gmail.com>
2016-02-22 19:49:03 +0000

    need to check individual methods and test it

--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -2,6 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Queen, type: :model do
   describe '#valid_move?' do
+
+    it 'return true if pieces is opponent' do
+      # this test is validated!2
+      game = FactoryGirl.create(:game)
+      queen = FactoryGirl.create(:queen, game: game )
+      FactoryGirl.create(:piece, row: 2, column: 5,color: "black", game: queen.game)
+      expect(queen.opponent_piece?(2,5)).to eq(true)
+    end
+
+    it 'return true if pieces is present' do
+      # this test needs working
+      game = FactoryGirl.create(:game)
+      queen = FactoryGirl.create(:queen, game: game )
+      FactoryGirl.create(:piece, row: 2, column: 5,color: "black", game: queen.game)
+      expect(queen.piece_present?(0, 2)).to eq(false)
+    end
+
     it 'return not_obstructed? false' do
       queen = FactoryGirl.create(:queen, row: 0, column: 3)
       FactoryGirl.create(:piece, row: 1, column: 4, game: queen.game)
